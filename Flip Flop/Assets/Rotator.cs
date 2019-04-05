45de6f9a25df9942691f864bfb813064f0ed93c5
Woodmanan <mwmccoy@andrew.cmu.edu>
2019-04-05 18:24:24 -0400

    beta

---
+++ Flip Flop/Assets/Rotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float mod;
+    private float rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mod > 0)
+        {
+            if (rot < 90)
+            {
+                rot += mod;
+            }
+            else
+            {
+                mod = -1 * mod;
+            }
+        }
+        else
+        {
+            if (rot > -90)
+            {
+                rot += mod;
+            }
+            else
+            {
+                mod = -1 * mod;
+            }
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+}
