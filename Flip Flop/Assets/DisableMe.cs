c2f73ad43324e10c5f094b8606585db1a384153f
Woodmanan <mwmccoy@andrew.cmu.edu>
2019-05-01 13:37:40 -0400

    Cursor Hotfix
    Oops. Changed 1 line, to remove the cursor.

--- Flip Flop/Assets/DisableMe.cs
+++ Flip Flop/Assets/DisableMe.cs
@@ -11,7 +11,7 @@ public class DisableMe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
